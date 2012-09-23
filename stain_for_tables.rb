19c91fb5b357bcd3ada4d739c752c3dceb6c41e6
Emma Lawler <emily.lawler@colorado.edu>
2012-09-23 16:24:11 -0600

    amount of stain needed for tables

---
+++ stain_for_tables.rb
@@ -0,0 +1,21 @@
+p the_length_of_one_side_of_the_table = 6
+
+p the_length_of_one_side_of_the_table * the_length_of_one_side_of_the_table = 36
+
+p total_surface_area_of_one_table = 36
+
+p number_of_tables = 4
+
+p total_surface_area_of_one_table * number_of_tables = 144
+
+surface_area_of_all_tables_in_room = 144
+
+coats_of_stain = 2
+
+coats_of_stain * surface_area_of_all_tables_in_room = 288
+
+Square_feet_of_stain_needed_to_coat_all_tables_twice = 288
+
+
+
+
